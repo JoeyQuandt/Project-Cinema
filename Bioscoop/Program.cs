af495726cfef92b28a1f6c7019104ae9c0f0dacd
mbvissers <mb.vissers@hotmail.nl>
2020-02-18 12:18:32 +0100

    Added basics for console app

---
+++ Bioscoop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bioscoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
