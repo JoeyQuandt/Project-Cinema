ed16d20ced5aaa4ab1bda69ceb564fc5cbf360b0
mbvissers <mb.vissers@hotmail.nl>
2020-03-02 09:40:45 +0100

    Added some basic classes and functionality in the program.cs

--- Bioscoop/Program.cs
+++ Bioscoop/Program.cs
@@ -6,7 +6,17 @@ namespace Bioscoop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Wat is jouw naam?");
+            // Create dummy actor objects
+            Actor joey = new Actor();
+            Actor thomas = new Actor();
+            // Put actors in array
+            Actor[] actors = { joey, thomas };
+
+            // Make movie object
+            Movie movie = new Movie("Joey's revenge", "A movie about the evil man Joey getting his revenge on Fransesco", 99, actors, DateTime.Now);
+
+            // Write the movie details from the movie object in the console
+            Console.WriteLine(movie.getMovieDetails());
         }
     }
 }
