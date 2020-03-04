d16932898a56a4a6dbf7e3ffe9e17c029cdd9e35
JoeyQuandt <57623466+JoeyQuandt@users.noreply.github.com>
2020-03-04 11:42:31 +0100

    movie
    movie data changed

---
+++ Bioscoop/Menu.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Menu
+{
+    private string menu_text;
+    private string movie_availability;
+    private string movie_prices;
+    private string account_information;
+}
