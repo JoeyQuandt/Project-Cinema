e78763792119f578d18b00104055d4324a59c2e2
JoeyQuandt <57623466+JoeyQuandt@users.noreply.github.com>
2020-03-09 13:47:37 +0100

    basic menu
    test

--- Bioscoop/Menu.cs
+++ Bioscoop/Menu.cs
@@ -1,19 +1,8 @@
-<<<<<<< HEAD
-using System;
-=======
-﻿using System;
->>>>>>> master
 
 public class Menu
 {
     private string menu_text;
-<<<<<<< HEAD
     private string movie_availability;
     private string movie_prices;
     private string account_information;
-=======
-    private string user_login;
-    private string movie_information;
-    private string buy_ticket;
->>>>>>> master
 }
