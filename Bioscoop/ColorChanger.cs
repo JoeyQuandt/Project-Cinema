775c17ba731bde78a9657a9fcf06bf6eea39ea02
Kimberly S <kimberlys99@icloud.com>
2020-06-10 16:22:07 +0200

    Netter

--- Bioscoop/ColorChanger.cs
+++ Bioscoop/ColorChanger.cs
@@ -4,10 +4,12 @@ using System.Text;
 
 public static class ColorChanger
 {
+    //Change textcolor
     public static void TextColor(ConsoleColor color)
     {
         Console.ForegroundColor = color;
     }
+    // Change backgroundcolor 
     public static void BackgroundColor(ConsoleColor color)
     {
         Console.BackgroundColor = color;
